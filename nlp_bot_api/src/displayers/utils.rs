db1a09d3fbf47d5ca2d124783875897d56b93029
Šimon Brandner <simon.bra.ag@gmail.com>
2024-03-20 11:23:24 +0100

    Simplify code
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- nlp_bot_api/src/displayers/utils.rs
+++ nlp_bot_api/src/displayers/utils.rs
@@ -1,12 +1,7 @@
-use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime};
+use chrono::{DateTime, NaiveDate};
 
 pub fn ngram_time_to_date(time: i64) -> NaiveDate {
-    let naive_datetime = NaiveDateTime::from_timestamp_opt(time, 0)
-        .expect("You are reaching a little to far into the future!");
-
-    #[allow(clippy::unwrap_used)]
-    let datetime: DateTime<FixedOffset> =
-        DateTime::from_naive_utc_and_offset(naive_datetime, FixedOffset::east_opt(0).unwrap());
-
-    datetime.date_naive()
+    DateTime::from_timestamp(time, 0)
+        .expect("You are reaching a little to far into the future!")
+        .date_naive()
 }
