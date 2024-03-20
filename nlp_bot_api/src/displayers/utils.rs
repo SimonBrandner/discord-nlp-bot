8121edc4cb7e9f792abeb3986dfd44ab787afe4c
Šimon Brandner <simon.bra.ag@gmail.com>
2024-03-20 11:19:36 +0100

    Revert "Update deps"
    This reverts commit 9ccd49fa00dec55d6baa8aa9e30ca8f9f4d01394.

--- nlp_bot_api/src/displayers/utils.rs
+++ nlp_bot_api/src/displayers/utils.rs
@@ -1,7 +1,12 @@
-use chrono::{DateTime, NaiveDate};
+use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime};
 
 pub fn ngram_time_to_date(time: i64) -> NaiveDate {
-    DateTime::from_timestamp(time, 0)
-        .expect("You are reaching a little to far into the future!")
-        .date_naive()
+    let naive_datetime = NaiveDateTime::from_timestamp_opt(time, 0)
+        .expect("You are reaching a little to far into the future!");
+
+    #[allow(clippy::unwrap_used)]
+    let datetime: DateTime<FixedOffset> =
+        DateTime::from_naive_utc_and_offset(naive_datetime, FixedOffset::east_opt(0).unwrap());
+
+    datetime.date_naive()
 }
